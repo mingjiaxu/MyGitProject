d8e94189dc938bdeb424d52433cf67d1a9cd621b
19542 <1954254451@qq.com>
2024-05-12 22:06:48 +0800

    增加了SPK计算过程中的一些基本方法，包括挑战T，摘要c 和 S

--- src/main/java/BaseFunc/SPK.java
+++ src/main/java/BaseFunc/SPK.java
@@ -1,6 +1,7 @@
 package BaseFunc;
 
 import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 /**
@@ -14,52 +15,106 @@ import java.util.ArrayList;
  */
 public class SPK {
     /**
-     * @param baseNumsList: 底数列表
-     * @param exponentsList: 指数列表
+     * @param baseNumList: 底数列表
+     * @param exponentList: 指数列表
      * @param m: 模数
      * @return BigInteger
      * @author xjm
      * @description 挑战计算模式一    型如 ax^rx *as ^rs....的挑战
      * @date 5/12/2024 8:56 PM
      */
-    public static BigInteger caculateT_P1(ArrayList<BigInteger> baseNumsList,ArrayList<BigInteger> exponentsList,BigInteger m) throws Exception {
-        if (baseNumsList == null)
+    public static BigInteger caculateT_P1(ArrayList<BigInteger> baseNumList,ArrayList<BigInteger> exponentList,BigInteger m) throws Exception {
+        if (baseNumList == null)
             throw new Exception("baseNums为空");
-        else if(exponentsList == null)
+        else if(exponentList == null)
             throw new Exception("baseNums为空");
-        else if(baseNumsList.size()!= exponentsList.size())
+        else if(baseNumList.size()!= exponentList.size())
             throw new Exception("底数列表与指数列表长度不相等");
         BigInteger T = BigInteger.ONE;
-        for (int i = 0; i < baseNumsList.size(); i++) {
-            T = T.multiply(baseNumsList.get(i).modPow(exponentsList.get(i),m));
+        for (int i = 0; i < baseNumList.size(); i++) {
+            T = T.multiply(baseNumList.get(i).modPow(exponentList.get(i),m));
         }
         return T.mod(m);
     }
     /**
-     * @param baseNumsList: 底数列表
-     * @param exponentsList: 指数列表
+     * @param baseNumList: 底数列表
+     * @param exponentList: 指数列表
      * @param m: 模数
      * @return BigInteger
      * @author xjm
      * @description 挑战计算模式二 型如  A^ry/(ax^x*as^s*....)的挑战
      * @date 5/12/2024 9:22 PM
      */
-    public static BigInteger caculateT_P2(ArrayList<BigInteger> baseNumsList,ArrayList<BigInteger> exponentsList,BigInteger m) throws Exception {
-        if (baseNumsList == null)
-            throw new Exception("baseNums为空");
-        else if(exponentsList == null)
-            throw new Exception("baseNums为空");
-        else if(baseNumsList.size()!= exponentsList.size())
-            throw new Exception("底数列表与指数列表长度不相等");
-        BigInteger T = baseNumsList.get(0).modPow(exponentsList.get(0),m);
-        ArrayList<BigInteger> baseNumsSubList = new ArrayList<BigInteger>();
-        ArrayList<BigInteger> exponentsSubList = new ArrayList<BigInteger>();
-        baseNumsSubList.addAll(1,baseNumsList);
-        exponentsSubList.addAll(1,exponentsList);
-        BigInteger invPart = caculateT_P1(baseNumsSubList,exponentsSubList,m).modInverse(m);
+    public static BigInteger caculateT_P2(ArrayList<BigInteger> baseNumList,ArrayList<BigInteger> exponentList,BigInteger m) throws Exception {
+        if (baseNumList == null)
+            throw new Exception("baseNumList为空");
+        else if(exponentList == null)
+            throw new Exception("exponentList为空");
+        else if(baseNumList.size()!= exponentList.size())
+            throw new Exception("baseNumList与exponentList长度不相等");
+        BigInteger T = baseNumList.get(0).modPow(exponentList.get(0),m);
+        ArrayList<BigInteger> baseNumSubList = new ArrayList<BigInteger>();
+        ArrayList<BigInteger> exponentSubList = new ArrayList<BigInteger>();
+        baseNumSubList.addAll(1,baseNumList);
+        exponentSubList.addAll(1,exponentList);
+        BigInteger invPart = caculateT_P1(baseNumSubList,exponentSubList,m).modInverse(m);
         return T.parallelMultiply(invPart).mod(m);
     }
-//    public static BigInteger caculateHashC(ArrayList<BigInteger> TList){
-//        return BigInteger.ONE;
-//    }
+    /**
+     * @param TList: 挑战的集合列表
+     * @return BigInteger
+     * @author xjm
+     * @description 对挑战的集合计算摘要;
+     * @date 5/12/2024 9:48 PM
+     */
+    public static BigInteger caculateHashC(ArrayList<BigInteger> TList) throws NoSuchAlgorithmException {
+        BigInteger T = BigInteger.ZERO;
+        for (int i = 0; i < TList.size(); i++) {
+            T = T.add(TList.get(i));
+        }
+        BigInteger c = GetHash.hash(T);
+        return c;
+    }
+    /**
+     * @param rList: 生成的随机数列表
+     * @param secParamList: 挑战中需要隐藏的秘密参数的列表
+     * @param c: 摘要
+     * @return ArrayList<BigInteger>
+     * @author xjm
+     * @description 计算S的集合
+     * @date 5/12/2024 9:59 PM
+     */
+    public static ArrayList<BigInteger> caculateSList(ArrayList<BigInteger> rList,ArrayList<BigInteger> secParamList,BigInteger c) throws Exception {
+        if(rList==null){
+            throw new Exception("rList为空");
+        }
+        else if(secParamList ==null){
+            throw new Exception("secParamList为空");
+        } else if (rList.size()!=secParamList.size()) {
+            throw new Exception("rList与secParamList长度不相等");
+        }
+        ArrayList<BigInteger> sList  = new ArrayList<BigInteger>();
+        for (int i = 0; i < rList.size() ; i++) {
+            sList.add(rList.get(i).subtract(c.parallelMultiply(secParamList.get(i))));
+        }
+        return sList;
+    }
+/**
+ * @param secParamList: 秘密参数列表
+ * @return ArrayList<BigInteger>
+ * @author xjm
+ * @description 为所有的秘密参数生成等长度的随机参数
+ * @date 5/12/2024 10:04 PM
+ */
+    public static ArrayList<BigInteger> generateRList(ArrayList<BigInteger> secParamList) throws Exception {
+        if(secParamList==null)
+            throw new Exception("secParamList为空");
+
+        ArrayList<BigInteger> rList = new ArrayList<BigInteger>();
+        for (int i = 0; i < secParamList.size() ; i++) {
+            rList.add(GetRndBigintger.generate(secParamList.get(i).bitLength()));
+        }
+        return rList;
+    }
+
 }
