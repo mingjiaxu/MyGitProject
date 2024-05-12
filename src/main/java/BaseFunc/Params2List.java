91bf2ba0d8240a344b9b5ffd386454c079992fb1
19542 <1954254451@qq.com>
2024-05-12 22:24:18 +0800

    增加了一个将变量转换为列表的方法，用于主函数代码量

---
+++ src/main/java/BaseFunc/Params2List.java
@@ -0,0 +1,112 @@
+package BaseFunc;
+
+import java.math.BigInteger;
+import java.util.ArrayList;
+
+/**
+ * @projectName: MyProject
+ * @package: BaseFunc
+ * @className: Params2List
+ * @author: xjm
+ * @description: 将参数加入列表
+ * @date: 5/12/2024 10:16 PM
+ * @version: 1.0
+ */
+public class Params2List {
+    /**
+     * @param a:
+     * @return ArrayList<BigInteger>
+     * @author xjm
+     * @description TODO
+     * @date 5/12/2024 10:20 PM
+     */
+    public static ArrayList<BigInteger> convert(BigInteger a){
+        ArrayList<BigInteger> list = new ArrayList<BigInteger>();
+        list.add(a);
+        return list;
+    }
+    /**
+     * @param a:
+     * @param b:
+     * @return ArrayList<BigInteger>
+     * @author xjm
+     * @description TODO
+     * @date 5/12/2024 10:21 PM
+     */
+    public static ArrayList<BigInteger> convert(BigInteger a,BigInteger b){
+        ArrayList<BigInteger> list = new ArrayList<BigInteger>();
+        list.add(a);
+        list.add(b);
+        return list;
+    }
+    /**
+     * @param a:
+     * @param b:
+     * @param c:
+     * @return ArrayList<BigInteger>
+     * @author xjm
+     * @description TODO
+     * @date 5/12/2024 10:21 PM
+     */
+    public static ArrayList<BigInteger> convert(BigInteger a,BigInteger b,BigInteger c){
+        ArrayList<BigInteger> list = new ArrayList<BigInteger>();
+        list.add(a);
+        list.add(b);
+        list.add(c);
+        return list;
+    }
+    /**
+     * @description 同上
+     * @date 5/12/2024 10:22 PM
+     */
+    public static ArrayList<BigInteger> convert(BigInteger a,BigInteger b,BigInteger c,BigInteger d){
+        ArrayList<BigInteger> list = new ArrayList<BigInteger>();
+        list.add(a);
+        list.add(b);
+        list.add(c);
+        list.add(d);
+        return list;
+    }
+    /**
+     * @description 同上
+     * @date 5/12/2024 10:22 PM
+     */
+    public static ArrayList<BigInteger> convert(BigInteger a,BigInteger b,BigInteger c,BigInteger d,BigInteger e){
+        ArrayList<BigInteger> list = new ArrayList<BigInteger>();
+        list.add(a);
+        list.add(b);
+        list.add(c);
+        list.add(d);
+        list.add(e);
+        return list;
+    }
+    /**
+     * @description 同上
+     * @date 5/12/2024 10:22 PM
+     */
+    public static ArrayList<BigInteger> convert(BigInteger a,BigInteger b,BigInteger c,BigInteger d,BigInteger e,BigInteger f){
+        ArrayList<BigInteger> list = new ArrayList<BigInteger>();
+        list.add(a);
+        list.add(b);
+        list.add(c);
+        list.add(d);
+        list.add(e);
+        list.add(f);
+        return list;
+    }
+    /**
+     * @description 同上
+     * @date 5/12/2024 10:22 PM
+     */
+    public static ArrayList<BigInteger> convert(BigInteger a,BigInteger b,BigInteger c,BigInteger d,BigInteger e,BigInteger f,BigInteger g){
+        ArrayList<BigInteger> list = new ArrayList<BigInteger>();
+        list.add(a);
+        list.add(b);
+        list.add(c);
+        list.add(d);
+        list.add(e);
+        list.add(f);
+        list.add(g);
+        return list;
+    }
+}
